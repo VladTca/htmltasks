ea32d6e70a7801ad4f6106f4749788689290e3a9
VladTca <votsats@gmail.com>
2023-12-26 14:28:02 +0100

    finish

--- hss.js
+++ hss.js
@@ -1,24 +1,29 @@
 const task = document.querySelector(".task");
+const button = document.querySelector(".parent").querySelector(".check");
+const input = document.querySelector(".input");
 
 const getRandomInRange = (min, max) => {
-    return Math.floor(min + Math.random() * (max - min + 1));
+  return Math.floor(min + Math.random() * (max - min + 1));
 };
+
 let firstRandomNumber = getRandomInRange(1, 9);
 let secondRandomNumber = getRandomInRange(1, 9);
 
 task.textContent = `${firstRandomNumber} * ${secondRandomNumber}`;
+
 button.addEventListener("click", () => {
-    let result = firstRandomNumber * secondRandomNumber;
-    let userAnswer = Number(input.value);
-    if (result === userAnswer) {
-        alert("Super");
-        firstRandomNumber = getRandomInRange(1, 9);
-        secondRandomNumber = getRandomInRange(1, 9);
-        task.textContent = `${firstRandomNumber} * 
-            ${secondRandomNumber}`;
-    } else {
-        alert("BAD");
-    }
-    input.value = ``;
+  let result = firstRandomNumber * secondRandomNumber;
+  let userAnswer = Number(input.value);
+  if (result === userAnswer) {
+    alert("Super");
+    firstRandomNumber = getRandomInRange(1, 9);
+    secondRandomNumber = getRandomInRange(1, 9);
+    task.textContent = `${firstRandomNumber} * ${secondRandomNumber}`;
+    input.value = "";
     input.focus();
+  } else {
+    alert("BAD");
+    input.value = "";
+    input.focus();
+  }
 });
